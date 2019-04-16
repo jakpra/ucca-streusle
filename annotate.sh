828fc48c9d446ab934d0a84e45983634d971d786
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-04-16 11:06:37 +0200

    Annotate as both array and extra

--- annotate.sh
+++ annotate.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-python -m semstr.scripts.annotate ../UCCA_English-EWT/sentences/train -u ../udpipe/models/english-ewt-ud-2.3-181115.udpipe -o udpipe/train -a
-python -m semstr.scripts.annotate ../UCCA_English-EWT/sentences/dev -u ../udpipe/models/english-ewt-ud-2.3-181115.udpipe -o udpipe/dev -a
-python -m semstr.scripts.annotate ../UCCA_English-EWT/sentences/test -u ../udpipe/models/english-ewt-ud-2.3-181115.udpipe -o udpipe/test -a
-
-python -m semstr.scripts.annotate ../UCCA_English-EWT/sentences/train -s en -o stanfordnlp/train -a
-python -m semstr.scripts.annotate ../UCCA_English-EWT/sentences/dev -s en -o stanfordnlp/dev -a
-python -m semstr.scripts.annotate ../UCCA_English-EWT/sentences/test -s en -o stanfordnlp/test -a
-
-python -m semstr.scripts.annotate ../UCCA_English-EWT/sentences/train -o spacy/train -a
-python -m semstr.scripts.annotate ../UCCA_English-EWT/sentences/dev -o spacy/dev -a
-python -m semstr.scripts.annotate ../UCCA_English-EWT/sentences/test -o spacy/test -a
-
-python -m semstr.scripts.annotate ../UCCA_English-EWT/sentences/train -c ../UD_English-EWT/en_ewt-ud-train.conllu -o goldud/train -a
-python -m semstr.scripts.annotate ../UCCA_English-EWT/sentences/dev -c ../UD_English-EWT/en_ewt-ud-dev.conllu -o goldud/dev -a
-python -m semstr.scripts.annotate ../UCCA_English-EWT/sentences/test -c ../UD_English-EWT/en_ewt-ud-test.conllu -o goldud/test -a
-
+wget https://raw.githubusercontent.com/danielhers/tupa/streusle-features/tupa/scripts/annotate_structural.py
+for div in train dev test; do
+  python -m semstr.scripts.annotate ../UCCA_English-EWT/sentences/$div -u ../udpipe/models/english-ewt-ud-2.3-181115.udpipe -o udpipe/$div -ae
+  python -m semstr.scripts.annotate ../UCCA_English-EWT/sentences/$div -s en -o stanfordnlp/$div -ae
+  python -m semstr.scripts.annotate ../UCCA_English-EWT/sentences/$div -o spacy/$div -ae
+  python -m semstr.scripts.annotate ../UCCA_English-EWT/sentences/$div -c ../UD_English-EWT/en_ewt-ud-$div.conllu -o goldud/$div -ae
+  python annotate_structural.py annotated/$div -f ../streusle/$div/xml -o annotated_structure/$div
+done
